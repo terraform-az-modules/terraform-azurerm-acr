4d3682a26fb8237e46cff2eadf761e3a7d4806d9
ayush <ayush.sharma@clouddrove.com>
2025-11-12 18:17:43 +0530

    fix: updated tags and source of modules

--- examples/complete/example.tf
+++ examples/complete/example.tf
@@ -10,8 +10,8 @@ data "azurerm_client_config" "current_client_config" {}
 ## Resource group in which all resources will be deployed.
 ##-----------------------------------------------------------------------------
 module "resource_group" {
-  source      = "terraform-az-modules/resource-group/azure"
-  version     = "1.0.1"
+  source      = "terraform-az-modules/resource-group/azurerm"
+  version     = "1.0.3"
   name        = "core"
   environment = "dev"
   location    = "centralus"
@@ -22,8 +22,8 @@ module "resource_group" {
 # Virtual Network
 # ------------------------------------------------------------------------------
 module "vnet" {
-  source              = "terraform-az-modules/vnet/azure"
-  version             = "1.0.0"
+  source              = "terraform-az-modules/vnet/azurerm"
+  version             = "1.0.3"
   name                = "core"
   environment         = "dev"
   label_order         = ["name", "environment", "location"]
@@ -36,8 +36,8 @@ module "vnet" {
 # Subnet
 # ------------------------------------------------------------------------------
 module "subnet" {
-  source               = "terraform-az-modules/subnet/azure"
-  version              = "1.0.0"
+  source               = "terraform-az-modules/subnet/azurerm"
+  version              = "1.0.1"
   environment          = "dev"
   label_order          = ["name", "environment", "location"]
   resource_group_name  = module.resource_group.resource_group_name
@@ -55,8 +55,8 @@ module "subnet" {
 # Log Analytics
 # ------------------------------------------------------------------------------
 module "log-analytics" {
-  source                      = "terraform-az-modules/log-analytics/azure"
-  version                     = "1.0.0"
+  source                      = "terraform-az-modules/log-analytics/azurerm"
+  version                     = "1.0.2"
   name                        = "core"
   environment                 = "dev"
   label_order                 = ["name", "environment", "location"]
@@ -70,8 +70,8 @@ module "log-analytics" {
 # Key Vault
 # ------------------------------------------------------------------------------
 module "vault" {
-  source                        = "terraform-az-modules/key-vault/azure"
-  version                       = "1.0.0"
+  source                        = "terraform-az-modules/key-vault/azurerm"
+  version                       = "1.0.1"
   name                          = "core"
   environment                   = "dev"
   label_order                   = ["name", "environment", "location"]
@@ -100,9 +100,12 @@ module "vault" {
 # Private DNS Zone
 # ------------------------------------------------------------------------------
 module "private_dns_zone" {
-  source              = "terraform-az-modules/private-dns/azure"
-  version             = "1.0.0"
+  source              = "terraform-az-modules/private-dns/azurerm"
+  version             = "1.0.2"
+  name                = "core"
+  environment         = "dev"
   resource_group_name = module.resource_group.resource_group_name
+  label_order         = ["name", "environment", "location"]
   private_dns_config = [
     {
       resource_type = "container_registry"
