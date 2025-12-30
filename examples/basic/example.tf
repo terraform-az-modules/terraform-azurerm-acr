e3529be5b10df1136bdaaf585a9831b0f69a9a46
Abhinav Singh Shekhawat <abhinav.shekhawat@clouddrove.com>
2025-12-30 20:43:38 +0530

    fix: updated automerge.yml, terraform-diff.yml (#12)

--- examples/basic/example.tf
+++ examples/basic/example.tf
@@ -12,6 +12,8 @@ module "container-registry" {
   label_order         = ["name", "environment", "location"]
   resource_group_name = "test"
   location            = "centralindia"
+  encryption          = false
+  enable_diagnostic   = false
   ##----------------------------------------------------------------------------- 
   ## To be mentioned for private endpoint, because private endpoint is enabled by default.
   ## To disable private endpoint set 'enable_private_endpoint' variable = false and than no need to specify following variable  
