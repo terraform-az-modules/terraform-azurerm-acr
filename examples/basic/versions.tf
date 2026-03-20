ed5f71c55efb4b34f602a3bb6e6cacc06323d162
Anmol Nagpal <anmolnagpal@users.noreply.github.com>
2026-03-20 15:50:16 -0400

    chore: polish module with basic example, changelog, and version fixes

--- examples/basic/versions.tf
+++ examples/basic/versions.tf
@@ -1,16 +1,10 @@
-##-----------------------------------------------------------------------------
-## Versions
-##-----------------------------------------------------------------------------
-# Terraform version
 terraform {
   required_version = ">= 1.14.5"
-}
 
-terraform {
   required_providers {
     azurerm = {
       source  = "hashicorp/azurerm"
-      version = ">=4.31.0"
+      version = "# Terraform version"
     }
   }
 }
